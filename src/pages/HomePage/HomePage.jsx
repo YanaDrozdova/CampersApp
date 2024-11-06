a2fb45d72e74f774118dabe0d7759d7480005f39
YanaDrozdova <yadro986@gmail.com>
2024-11-06 17:10:12 +0200

    added home page, header, fonts

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,1 +1,22 @@
-export default function HomePage() {}
+import { useNavigate } from 'react-router-dom';
+import css from './HomePage.module.css';
+
+export default function HomePage() {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate('/catalog');
+  };
+  return (
+    <main>
+      <div className={css.container}>
+        <h1 className={css.title}>Campers of your dreams</h1>
+        <h2 className={css.text}>
+          You can find everything you want in our catalog
+        </h2>
+        <button className={css.viewBtn} type="button" onClick={handleClick}>
+          View Now
+        </button>
+      </div>
+    </main>
+  );
+}
