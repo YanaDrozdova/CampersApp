af2f7a60e4cedeff93d29c2cba4b3a87d0d9aa0e
YanaDrozdova <yadro986@gmail.com>
2024-11-10 15:44:36 +0200

    added html,css of Reviews component

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,3 +1,95 @@
-// import css from './Reviews.module.css';
+import css from './Reviews.module.css';
 
-export default function Reviews() {}
+export default function Reviews() {
+  return (
+    <div className={css.container}>
+      <ul className={css.reviewsList}>
+        <li className={css.reviewsItem}>
+          <div className={css.reviewerTitle}>
+            <div className={css.reviewerAvatar}>
+              <p className={css.avatarText}>A</p>
+            </div>
+            <div className={css.reviewerInfo}>
+              <p className={css.reviewerName}>Alice</p>
+              <ul className={css.starList}>
+                <li className={css.starItem}>
+                  <svg width={'16px'} height={'16px'} className={css.iconStar}>
+                    <use href={`/src/assets/images/icons-defs.svg#icon-star`} />
+                  </svg>
+                </li>
+                <li className={css.starItem}>
+                  <svg width={'16px'} height={'16px'} className={css.iconStar}>
+                    <use href={`/src/assets/images/icons-defs.svg#icon-star`} />
+                  </svg>
+                </li>
+                <li className={css.starItem}>
+                  <svg width={'16px'} height={'16px'} className={css.iconStar}>
+                    <use href={`/src/assets/images/icons-defs.svg#icon-star`} />
+                  </svg>
+                </li>
+                <li className={css.starItem}>
+                  <svg width={'16px'} height={'16px'} className={css.iconStar}>
+                    <use href={`/src/assets/images/icons-defs.svg#icon-star`} />
+                  </svg>
+                </li>
+                <li className={css.starItem}>
+                  <svg width={'16px'} height={'16px'} className={css.iconStar}>
+                    <use href={`/src/assets/images/icons-defs.svg#icon-star`} />
+                  </svg>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <p>
+            The Mavericks panel truck was a perfect choice for my solo road
+            trip. Compact, easy to drive, and had all the essentials. The
+            kitchen facilities were sufficient, and the overall experience was
+            fantastic.
+          </p>
+        </li>
+        <li className={css.reviewsItem}>
+          <div className={css.reviewerTitle}>
+            <div className={css.reviewerAvatar}>
+              <p className={css.avatarText}>B</p>
+            </div>
+            <div className={css.reviewerInfo}>
+              <p className={css.reviewerName}>Bob</p>
+              <ul className={css.starList}>
+                <li className={css.starItem}>
+                  <svg width={'16px'} height={'16px'} className={css.iconStar}>
+                    <use href={`/src/assets/images/icons-defs.svg#icon-star`} />
+                  </svg>
+                </li>
+                <li className={css.starItem}>
+                  <svg width={'16px'} height={'16px'} className={css.iconStar}>
+                    <use href={`/src/assets/images/icons-defs.svg#icon-star`} />
+                  </svg>
+                </li>
+                <li className={css.starItem}>
+                  <svg width={'16px'} height={'16px'} className={css.iconStar}>
+                    <use href={`/src/assets/images/icons-defs.svg#icon-star`} />
+                  </svg>
+                </li>
+                <li className={css.starItem}>
+                  <svg width={'16px'} height={'16px'} className={css.iconStar}>
+                    <use href={`/src/assets/images/icons-defs.svg#icon-star`} />
+                  </svg>
+                </li>
+                <li className={css.starItem}>
+                  <svg width={'16px'} height={'16px'} className={css.iconStar}>
+                    <use href={`/src/assets/images/icons-defs.svg#icon-star`} />
+                  </svg>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <p>
+            A decent option for solo travel. The Mavericks provided a
+            comfortable stay, but the lack of bathroom facilities was a
+            drawback. Good for short trips where simplicity is preferred.
+          </p>
+        </li>
+      </ul>
+    </div>
+  );
+}
