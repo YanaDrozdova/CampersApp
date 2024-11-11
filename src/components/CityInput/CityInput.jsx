d5eca685bd526140e9a1b65eb8ee4569bc9311f1
YanaDrozdova <yadro986@gmail.com>
2024-11-11 20:26:52 +0200

    added city input component

--- src/components/CityInput/CityInput.jsx
+++ src/components/CityInput/CityInput.jsx
@@ -1,1 +1,39 @@
-export default function CityInput() {}
+import { useState } from 'react';
+import css from './CityInput.module.css';
+import clsx from 'clsx';
+
+export default function CityInput() {
+  const [location, setLocation] = useState('');
+
+  const handleLocationChange = e => {
+    console.log(e.target.value);
+    setLocation(e.target.value);
+    // Логіка фільтрації кемперів
+  };
+  return (
+    <div className={css.container}>
+      <p className={css.text}>Location</p>
+      <div className={css.iconWrapper}>
+        <svg width={'20px'} height={'20px'} className={css.iconMap}>
+          <use href={`/images/icons-defs.svg#icon-map`} />
+        </svg>
+        <select
+          value={location}
+          onChange={handleLocationChange}
+          className={clsx(css.select, location === '' && css.placeholder)}
+        >
+          <option value="" disabled>
+            City
+          </option>
+          <option value="Dnipro">Dnipro, Ukraine</option>
+          <option value="Kharkiv">Kharkiv, Ukraine</option>
+          <option value="Kyiv">Kyiv, Ukraine</option>
+          <option value="Lviv">Lviv, Ukraine</option>
+          <option value="Odesa">Odesa, Ukraine</option>
+          <option value="Poltava">Poltava, Ukraine</option>
+          <option value="Sumy">Sumy, Ukraine</option>
+        </select>
+      </div>
+    </div>
+  );
+}
