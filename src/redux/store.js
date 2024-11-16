d4886d24b088d1e1de9950eb520355dc96350591
YanaDrozdova <yadro986@gmail.com>
2024-11-16 12:27:28 +0200

    added persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,39 @@
 import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import filtersReducer from './filters/filtersSlice.js';
 import campersReducer from './campers/campersSlice.js';
 
+const persistedCampersReducer = persistReducer(
+  {
+    key: 'camper-info',
+    storage,
+    whitelist: ['camperInfo'],
+  },
+  campersReducer
+);
+
 export const store = configureStore({
   reducer: {
-    campers: campersReducer,
+    campers: persistedCampersReducer,
     filters: filtersReducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
